59ecb45423a75270427f20ec1f175a3cd0fae391
saifyedes <saifyedes20@gmail.com>
2026-01-22 15:02:59 +0100

    sauvegarde avec de passer au version cordova

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,17 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import java.util.ArrayList;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Register Cordova plugins manually (REQUIRED)
+        registerPlugin(com.crypho.plugins.SecureStorage.class);
+    }
+}
